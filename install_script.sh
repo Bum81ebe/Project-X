d5e49a099ac0cd8b2c464f4c5c88de9a4e9e6a2c
Bumb1ebe <86529395+Bum81ebe@users.noreply.github.com>
2023-10-22 17:18:16 +0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# Update the system and install necessary utilities
-sudo apt-get update && sudo apt-get install -y curl build-essential && sudo apt install dnsutils && sudo apt-get install net-tools && sudo apt-get install tcpdump && sudo apt-get install dsniff -y && sudo apt install grepcidr && sudo apt install unzip
+set -e  # Exit immediately if a command exits with a non-zero status.
 
+# Update the system
+sudo apt-get update
+
+# Install necessary utilities
+sudo apt-get install -y curl build-essential dnsutils net-tools tcpdump dsniff grepcidr unzip
 
 # Install dos deflate
 wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
 unzip ddos.zip
+
+if [ ! -d "ddos-deflate-master" ]; then
+    echo "Error: Directory ddos-deflate-master does not exist."
+    exit 1
+fi
+
 cd ddos-deflate-master
 ./install.sh
 
-# Note: The above commands change the number of connections, email, and ban period.
-# You can adjust the sed commands to modify other parameters in the configuration file.
-
+# Enable and start the ddos service
 sudo systemctl enable ddos
 sudo systemctl start ddos
 
@@ -20,7 +28,11 @@ sudo systemctl start ddos
 sudo apt-get install -y haproxy
 
 # Verify installations
-which haproxy
+if ! which haproxy > /dev/null; then
+    echo "Error: haproxy installation failed."
+    exit 1
+fi
+
 /usr/sbin/ddos --version
 
 echo "Installation and configuration complete!"
