6b10e8fc2c036079fd9b289b8f3e1db9903c0813
Bumb1ebe <86529395+Bum81ebe@users.noreply.github.com>
2023-10-22 16:21:35 +0400

    Add files via upload

---
+++ install_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update the system and install necessary utilities
+sudo apt-get update && sudo apt-get install -y curl build-essential
+
+sudo apt install dnsutils
+sudo apt-get install net-tools
+sudo apt-get install tcpdump
+sudo apt-get install dsniff -y
+sudo apt install grepcidr
+
+# Install dos deflate
+wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
+unzip ddos.zip
+cd ddos-deflate-master
+./install.sh
+
+# Note: The above commands change the number of connections, email, and ban period.
+# You can adjust the sed commands to modify other parameters in the configuration file.
+
+sudo systemctl enable ddos
+sudo systemctl start ddos
+
+# Install haproxy
+sudo apt-get install -y haproxy
+
+# Verify installations
+which haproxy
+/usr/sbin/ddos --version
+
+echo "Installation and configuration complete!"
